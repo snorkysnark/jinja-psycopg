2508831c97639fc2388106cbfd723f38a1201a03
francisthebasilisk <snorkysnark@gmail.com>
2022-07-14 21:15:26 +0300

    More readable tests

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -15,33 +15,30 @@ def conn():
 def test_args(conn: Connection):
     query = "SELECT * FROM {{ table }} WHERE id = {{ id }}"
     expected = 'SELECT * FROM "sources" WHERE id = 5'
+    params = {"table": sql.Identifier("sources"), "id": 5}
 
-    assert (
-        JinjaPsycopg()
-        .render(query, {"table": sql.Identifier("sources"), "id": 5})
-        .as_string(conn)
-        == expected
-    )
+    assert JinjaPsycopg().render(query, params).as_string(conn) == expected
 
 
 def test_no_args(conn: Connection):
     query = "SELECT * FROM aaa"
 
-    assert JinjaPsycopg().render(query, {}).as_string(conn) == query
+    assert JinjaPsycopg().render(query).as_string(conn) == query
 
 
 def test_string(conn: Connection):
-    query = "{{ 'foo' }}"
-    expected = "'foo'"
+    query = "WHERE name = {{ 'foo' }}"
+    expected = "WHERE name = 'foo'"
 
-    assert JinjaPsycopg().render(query, {}).as_string(conn) == expected
+    assert JinjaPsycopg().render(query).as_string(conn) == expected
 
 
 def test_string_order(conn: Connection):
-    query = "{{ foo }}, {{ 'bar' }}"
-    expected = "'foo', 'bar'"
+    query = "WHERE name = {{ foo }}, value = {{ 'bar' }}"
+    expected = "WHERE name = 'foo', value = 'bar'"
+    params = {"foo": "foo"}
 
-    assert JinjaPsycopg().render(query, {"foo": "foo"}).as_string(conn) == expected
+    assert JinjaPsycopg().render(query, params).as_string(conn) == expected
 
 
 class CustomRenderer(JinjaPsycopg):
@@ -55,14 +52,15 @@ class CustomRenderer(JinjaPsycopg):
 
 
 def test_function(conn: Connection):
-    query = "{{ uppercase('bar') }}"
-    expected = "'BAR'"
+    query = "VALUES ( {{ uppercase('bar') }} )"
+    expected = "VALUES ( 'BAR' )"
 
-    assert CustomRenderer().render(query, {}).as_string(conn) == expected
+    assert CustomRenderer().render(query).as_string(conn) == expected
 
 
 def test_function_order(conn: Connection):
-    query = "{{ foo }}, {{ uppercase('bar') }}"
-    expected = "'foo', 'BAR'"
+    query = "VALUES ( {{ foo }}, {{ uppercase('bar') }} )"
+    expected = "VALUES ( 'foo', 'BAR' )"
+    params = {"foo": "foo"}
 
-    assert CustomRenderer().render(query, {"foo": "foo"}).as_string(conn) == expected
+    assert CustomRenderer().render(query, params).as_string(conn) == expected
