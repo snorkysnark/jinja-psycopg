ee19d498296f87fc89ce1a861a09ffd48d769a14
francisthebasilisk <snorkysnark@gmail.com>
2022-07-14 19:13:41 +0300

    test for custom function

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -1,15 +1,8 @@
-from textwrap import dedent
-
 import pytest
 import psycopg
 from psycopg import Connection, sql
 
 from jinja_psycopg import JinjaPsycopg
-
-
-@pytest.fixture
-def renderer():
-    return JinjaPsycopg()
 
 
 @pytest.fixture
@@ -19,33 +12,57 @@ def conn():
     connection.close()
 
 
-def test_args(renderer: JinjaPsycopg, conn: Connection):
+def test_args(conn: Connection):
     query = "SELECT * FROM {{ table }} WHERE id = {{ id }}"
     expected = 'SELECT * FROM "sources" WHERE id = 5'
 
     assert (
-        renderer.render(query, {"table": sql.Identifier("sources"), "id": 5}).as_string(
-            conn
-        )
+        JinjaPsycopg()
+        .render(query, {"table": sql.Identifier("sources"), "id": 5})
+        .as_string(conn)
         == expected
     )
 
 
-def test_no_args(renderer: JinjaPsycopg, conn: Connection):
+def test_no_args(conn: Connection):
     query = "SELECT * FROM aaa"
 
-    assert renderer.render(query, {}).as_string(conn) == query
+    assert JinjaPsycopg().render(query, {}).as_string(conn) == query
 
 
-def test_string(renderer: JinjaPsycopg, conn: Connection):
+def test_string(conn: Connection):
     query = "{{ 'foo' }}"
     expected = "'foo'"
 
-    assert renderer.render(query, {}).as_string(conn) == expected
+    assert JinjaPsycopg().render(query, {}).as_string(conn) == expected
 
 
-def test_string_order(renderer: JinjaPsycopg, conn: Connection):
+def test_string_order(conn: Connection):
     query = "{{ foo }}, {{ 'bar' }}"
     expected = "'foo', 'bar'"
 
-    assert renderer.render(query, {"foo": "foo"}).as_string(conn) == expected
+    assert JinjaPsycopg().render(query, {"foo": "foo"}).as_string(conn) == expected
+
+
+class CustomRenderer(JinjaPsycopg):
+    def _prepare_environment(self):
+        super()._prepare_environment()
+
+        def uppercase(string: str):
+            return string.upper()
+
+        self.env.globals["uppercase"] = uppercase
+
+
+def test_function(conn: Connection):
+    query = "{{ uppercase('bar') }}"
+    expected = "'BAR'"
+
+    assert CustomRenderer().render(query, {}).as_string(conn) == expected
+
+
+def test_function_order(conn: Connection):
+    query = "{{ foo }}, {{ uppercase('bar') }}"
+    expected = "'foo', 'BAR'"
+
+    assert CustomRenderer().render(query, {"foo": "foo"}).as_string(conn) == expected
